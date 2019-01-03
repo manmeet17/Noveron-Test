47f0e2261ef867ab5262af66a62ac59bef16cd8d
Manmeet Tarun <meetutarun16@gmail.com>
2019-01-04 03:55:43 +0530

    Data Loader added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,8 +9,8 @@ $(function () {
                 var row = $('<tr></tr>');
                 var th = $('<th></th>').attr('scope', 'row').text(i + 1).appendTo(row);
                 var keys = Object.keys(data[i]);
-                for (var j = 1; j < keys.length-1; j++) {
-                    row.append('<td>'+data[i][keys[j]]+'</td>');
+                for (var j = 1; j < keys.length - 1; j++) {
+                    row.append('<td>' + data[i][keys[j]] + '</td>');
                 }
                 // console.log(row);                
                 head.append(row);
@@ -18,13 +18,28 @@ $(function () {
         }
     });
 
-    $('#search').keyup(function() {
+    $('#search').keyup(function () {
         var $rows = $('tbody tr');
         var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-        
-        $rows.show().filter(function() {
+
+        $rows.show().filter(function () {
             var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
             return !~text.indexOf(val);
         }).hide();
+    });
+
+    $('.submitBtn').click(function (e) {
+        var data = $('#newEntryForm').serialize();
+        $.ajax({
+            type: "POST",
+            url: '/tenant/new',
+            data: data,
+            success: function (res) {
+                console.log(res);
+                window.location.href="/";
+            }
+        });
+
+        e.preventDefault();
     });
 });
